6d4c2595479ba071daf1910dd70753ccd49418d1
Vukašin Manojlović <vmanojlovic@monad.foundation>
2025-12-18 13:45:02 +0100

    Keep only TxnLog filters

--- frontend/types/events.ts
+++ frontend/types/events.ts
@@ -191,54 +191,7 @@ interface ArrayPrefixFilter<T> {
 }
 
 export type FieldFilter =
-  | { field: 'block_number'; filter: RangeFilter<number> }
   | { field: 'txn_index'; filter: RangeFilter<number> }
-  | { field: 'seqno'; filter: RangeFilter<number> }
-  | { field: 'timestamp_ns'; filter: RangeFilter<number> }
-  | { field: 'error_type'; filter: ExactMatchFilter<number> }
-  | { field: 'dropped_event_type'; filter: ExactMatchFilter<number> }
-  | { field: 'truncated_payload_size'; filter: ExactMatchFilter<number> }
-  | { field: 'requested_payload_size'; filter: RangeFilter<number> }
-  | { field: 'block_id'; filter: ExactMatchFilter<string> }
-  | { field: 'round'; filter: RangeFilter<number> }
-  | { field: 'epoch'; filter: RangeFilter<number> }
-  | { field: 'parent_eth_hash'; filter: ExactMatchFilter<string> }
-  | { field: 'timestamp'; filter: RangeFilter<number> }
-  | { field: 'beneficiary'; filter: ExactMatchFilter<string> }
-  | { field: 'gas_limit'; filter: RangeFilter<number> }
-  | { field: 'base_fee_per_gas'; filter: RangeFilter<string> }
-  | { field: 'reason'; filter: ExactMatchFilter<number> }
-  | { field: 'eth_block_hash'; filter: ExactMatchFilter<string> }
-  | { field: 'state_root'; filter: ExactMatchFilter<string> }
-  | { field: 'receipts_root'; filter: ExactMatchFilter<string> }
-  | { field: 'gas_used'; filter: RangeFilter<number> }
-  | { field: 'txn_index'; filter: RangeFilter<number> }
-  | { field: 'txn_hash'; filter: ExactMatchFilter<string> }
-  | { field: 'sender'; filter: ExactMatchFilter<string> }
-  | { field: 'txn_type'; filter: ExactMatchFilter<number> }
-  | { field: 'chain_id'; filter: ExactMatchFilter<string> }
-  | { field: 'nonce'; filter: RangeFilter<number> }
-  | { field: 'max_fee_per_gas'; filter: RangeFilter<string> }
-  | { field: 'max_priority_fee_per_gas'; filter: RangeFilter<string> }
-  | { field: 'value'; filter: RangeFilter<string> }
-  | { field: 'to'; filter: ExactMatchFilter<string> }
-  | { field: 'is_contract_creation'; filter: ExactMatchFilter<boolean> }
-  | { field: 'access_list_count'; filter: ExactMatchFilter<number> }
-  | { field: 'auth_list_count'; filter: ExactMatchFilter<number> }
-  | { field: 'access_list_address'; filter: ExactMatchFilter<string> }
   | { field: 'address'; filter: ExactMatchFilter<string> }
-  | { field: 'log_count'; filter: ExactMatchFilter<number> }
-  | { field: 'status'; filter: ExactMatchFilter<boolean> }
   | { field: 'log_index'; filter: RangeFilter<number> }
   | { field: 'topics'; filter: ArrayPrefixFilter<string> }
-  | { field: 'depth'; filter: RangeFilter<number> }
-  | { field: 'caller'; filter: ExactMatchFilter<string> }
-  | { field: 'call_target'; filter: ExactMatchFilter<string> }
-  | { field: 'entry_count'; filter: ExactMatchFilter<number> }
-  | { field: 'balance'; filter: RangeFilter<string> }
-  | { field: 'code_hash'; filter: ExactMatchFilter<string> }
-  | { field: 'account_index'; filter: RangeFilter<number> }
-  | { field: 'storage_key'; filter: ExactMatchFilter<string> }
-  | { field: 'storage_value'; filter: ExactMatchFilter<string> }
-  | { field: 'domain_id'; filter: RangeFilter<number> }
-  | { field: 'status_code'; filter: RangeFilter<number> }
