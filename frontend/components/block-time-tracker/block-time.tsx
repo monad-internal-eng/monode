f6bc5617b4b59c124619cd8f6413ea85bf161787
Camillebzd <48495021+Camillebzd@users.noreply.github.com>
2026-01-15 11:20:27 +0000

    fix(frontend): make external block number on Block Execution Timeline a link (#63)
    * external block number on Block Execution Timeline is an external link
    
    * hover text color white

--- frontend/components/block-time-tracker/block-time.tsx
+++ frontend/components/block-time-tracker/block-time.tsx
@@ -165,9 +165,12 @@ export const BlockTime = ({
       <div className="w-full h-px bg-zinc-700" />
 
       {/* Block number Label */}
-      <div className="text-sm font-medium text-zinc-600">
+      <ExternalLink
+        href={`${EXPLORER_URL}/block/${block.number}`}
+        className="text-sm font-medium text-zinc-600 hover:text-white"
+      >
         {formatBlockNumber(block.number)}
-      </div>
+      </ExternalLink>
     </div>
   )
 }
